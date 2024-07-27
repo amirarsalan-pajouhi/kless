331f30b835f1fe017bc4925c698fd635b00155b0
Amirarsalan <amirpzh04@gmail.com>
2024-07-27 20:09:05 +0300

    12

--- src/components/Admin.tsx
+++ src/components/Admin.tsx
@@ -141,7 +141,7 @@ export default function Admin() {
           className="absolute bottom-4 left-4 px-4 py-2 bg-gray-500 text-white rounded-md hover:bg-gray-700 focus:outline-none focus:ring-2 focus:ring-gray-500 focus:ring-opacity-50"
         >
           Logout
-        </button>
+        </button> 
       </div>
     </section>
   );
