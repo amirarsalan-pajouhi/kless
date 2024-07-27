c3f43815934d3c3fdb871ac3e8a3ef587629caa0
Amirarsalan <amirpzh04@gmail.com>
2024-07-27 20:05:21 +0300

    2

--- src/firebase.ts
+++ src/firebase.ts
@@ -1,6 +1,7 @@
 import { initializeApp } from "firebase/app";
-import { getAuth } from "firebase/auth";
+import { getAuth, setPersistence, browserLocalPersistence } from "firebase/auth";
 import { getFirestore } from "firebase/firestore";
+
 const firebaseConfig = {
   apiKey: "AIzaSyBF4vX12zP594POJkq7NzrVadNijlQul4E",
   authDomain: "qless-a48d3.firebaseapp.com",
@@ -10,6 +11,16 @@ const firebaseConfig = {
   appId: "1:1086384840612:web:59bad6b45a90171971ab31",
   measurementId: "G-M2RC9VXK3V",
 };
+
+// Initialize Firebase
 const app = initializeApp(firebaseConfig);
+
+// Initialize Firestore and Auth
 export const db = getFirestore(app);
 export const auth = getAuth(app);
+
+// Set persistence
+setPersistence(auth, browserLocalPersistence)
+  .catch((error) => {
+    console.error("Error setting persistence:", error);
+  });
